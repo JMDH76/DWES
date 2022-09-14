0b9704e936aa8dffbcc02eb6f6266ee9496a14a6
J_M_Dominguez <jmdominguezhernandez@gmail.com>
2022-09-14 09:56:54 +0200

    AP00. Finalizada

---
+++ AP00_Ejemplo_teoria/model.php
@@ -0,0 +1,12 @@
+<?php
+/* MODELO: array con los datos a presentar en "view" cuando se haga la
+solicitud desde "controler" hace de BDD */
+ 
+$diccionario = array(
+        'page_title'=>'AP00. MVC Ejemplo teoría',
+        'keywords'=>'poo, mvc, php, arquitectura de software',
+        'description'=>'Actividad práctica AP00 MVC Ejemplo teoría',
+        'content'=>'En un lugar de la Mancha cuyo nombre no quiero acordar, vivia un caballero de los de lanza...'  
+        );   
+          
+?>
